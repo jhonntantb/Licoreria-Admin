9bce5b3da75c233d44e31afcfb9d15c724321dff
Jhonatan Tabraj <jhonntantb@gmail.com>
2022-07-18 14:59:49 -0500

    project structure

---
+++ src/App.js
@@ -0,0 +1,12 @@
+/* eslint-disable react/react-in-jsx-scope */
+import './App.css';
+
+function App() {
+  return (
+    <h1 className="underline text-3xl">
+      App
+    </h1>
+  );
+}
+
+export default App;
